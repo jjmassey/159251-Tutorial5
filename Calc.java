ac75db3d4c1b065dce2eb493f7b4568853c84466
jjmassey <joshua.fleet.1@uni.massey.ac.nz>
2018-09-13 15:46:17 +1200

    y

--- Calc.java
+++ Calc.java
@@ -4,7 +4,7 @@ public class Calc {
         return a - b;
         }
 
-    public static void main(String [] args) {
+    public static void main(String[] args) {
         Calc c = new Calc();
         assert c.add(3,3) == 6;
     }
