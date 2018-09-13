a6d8491f23236633c5db183c5f01bbbb37d6e900
jjmassey <joshua.fleet.1@uni.massey.ac.nz>
2018-09-13 13:20:31 +1200

    Fixed copy and paste errors

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/ bin / bash
 
-rm *. class # clean project
-javac Calc . java # compile project
+rm *.class # clean project
+javac Calc.java # compile project
 if [ $? -ne 0 ] # if exit code is not 0 javac failed
 then
     echo " Compile failed !"
